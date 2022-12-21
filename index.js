162895f1d276e152241786a055dd07df6fead00e
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-20 22:02:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function initMap() {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: { lat:40.8075, lng: -73.9626 },
+    });
+  
+    directionsRenderer.setMap(map);
+  
+    const onChangeHandler = function () {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+  
+    document.getElementById("start").addEventListener("change", onChangeHandler);
+    document.getElementById("end").addEventListener("change", onChangeHandler);
+  }
+  
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    directionsService
+      .route({
+        origin: {
+          query: document.getElementById("start").value,
+        },
+        destination: {
+          query: document.getElementById("end").value,
+        },
+        travelMode: google.maps.TravelMode.WALKING,
+      })
+      .then((response) => {
+        directionsRenderer.setDirections(response);
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
+  }
+  
+  window.initMap = initMap;
+  
