701f0f34199697487daa415a751255a1bd5e1078
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-29 20:42:43 -0500

    more adjustments

--- index.js
+++ index.js
@@ -31,11 +31,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
       directionsRenderer.setDirections(response);
       const duration = response.routes[0].legs[0].duration;
       console.log(`Estimated travel time: ${duration.text}`);
-      
-    })
-    .catch((e) => window.alert("Directions request failed due to " + status));
-}
-const durationElement = document.getElementById("duration");
+      const durationElement = document.getElementById("duration");
       durationElement.innerHTML = `Estimated Travel Time: ${duration.text}`;
+    })
+}
 
 window.initMap = initMap;
